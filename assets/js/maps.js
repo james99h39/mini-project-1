3c508935ca2139979ec74dc36ff7f9f87e6982d1
James Horne <james99h39@gmail.com>
2019-01-24 12:10:59 +0000

    updated index.html, contact.html, github.html and maps.js files

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,9 +1,9 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 8,
+        zoom: 9,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 41.01592,
+            lng: -73.929282
         }
     });
     
